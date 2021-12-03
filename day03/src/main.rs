e173fe0696741383f23d186e9cfa29e9c874d34d
Sander van Kasteel <22967311+svankasteel@users.noreply.github.com>
2021-12-03 15:22:47 +0100

    refactor day03

--- day03/src/main.rs
+++ day03/src/main.rs
@@ -42,38 +42,25 @@ fn gamma_rate_from(baa: Vec<Vec<u8>>) -> Vec<u8> {
 
     baa.iter().for_each(|ba| ba.iter().enumerate().for_each(|(i, v)| result_vec[i] += *v as i32));
 
-    let q = result_vec.iter().map(|i| (i >= &threshold) as u8).collect::<Vec<u8>>();
-
-    println!("gr: {}", q.iter().map(|x| x.to_string()).collect::<Vec<String>>().join(""));
-
-    return q
+    return result_vec.iter().map(|i| (i >= &threshold) as u8).collect::<Vec<u8>>();
 }
 
 fn epsilon_rate_from(baa: Vec<Vec<u8>>) -> Vec<u8> {
-    let threshold: i32 = (baa.len() as f32 / 2.0).ceil() as i32;
-    let mut result_vec: Vec<i32> = Vec::new();
-
-    for _i in 0..baa[0].len() {
-        result_vec.push(0)
-    }
-
-    baa.iter().for_each(|ba| ba.iter().enumerate().for_each(|(i, v)| result_vec[i] += *v as i32));
-
-    return result_vec.iter().map(|i| !(i >= &threshold) as u8).collect::<Vec<u8>>();
+    return gamma_rate_from(baa).iter().map(
+        |b| 1 - b
+    ).collect()
 }
 
-fn find_ox_gen_rating(baa: Vec<Vec<u8>>) -> Vec<u8> {
+fn find_rating_with_fn(baa: Vec<Vec<u8>>, f: &dyn Fn(Vec<Vec<u8>>) -> Vec<u8>) -> Vec<u8> {
     let mut res: Vec<Vec<u8>> = baa.to_vec();
 
     for i in 0..baa[0].len() {
-        let gr = gamma_rate_from(res.to_vec()).to_vec();
+        let gr = f(res.to_vec()).to_vec();
 
         let res1: Vec<Vec<u8>> = res.iter().filter(
             |ba| ba[i] == gr[i]
         ).cloned().collect();
 
-        println!("{:?}", res1);
-
         if res1.len() == 1 {
             return res1[0].to_vec();
         }
@@ -84,22 +71,10 @@ fn find_ox_gen_rating(baa: Vec<Vec<u8>>) -> Vec<u8> {
     return [].to_vec();
 }
 
+fn find_ox_gen_rating(baa: Vec<Vec<u8>>) -> Vec<u8> {
+    return find_rating_with_fn(baa, &gamma_rate_from)
+}
+
 fn find_co2_scrub_rating(baa: Vec<Vec<u8>>) -> Vec<u8> {
-    let mut res: Vec<Vec<u8>> = baa.to_vec();
-
-    for i in 0..baa[0].len() {
-        let er = epsilon_rate_from(res.to_vec()).to_vec();
-
-        let res1: Vec<Vec<u8>> = res.iter().filter(
-            |ba| ba[i] == er[i]
-        ).cloned().collect();
-
-        if res1.len() == 1 {
-            return res1[0].to_vec();
-        }
-
-        res = res1
-    }
-
-    return [].to_vec();
+    return find_rating_with_fn(baa, &epsilon_rate_from)
 }
